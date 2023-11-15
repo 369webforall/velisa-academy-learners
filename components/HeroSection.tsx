04c36be83ecea8f2e4009e20b807e3ae2f9954e2
Your Name <lebomariri505@gmail.com>
2023-11-15 14:02:59 +0200

    Updated Styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,29 +3,30 @@ import { Grid, Box, Container, Flex } from '@radix-ui/themes';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
+import Hero from '../app/assets/hero8.png'
 const HeroSection = () => {
   return (
     <Container className="p-4">
-      <Grid columns={{ initial: '1', md: '2' }} gap="8">
+      <Grid columns={{ initial: '1', md: '2' }} gap="8" >
         <Box>
-          <h1 className="text-2xl text-purple-600 lg:text-3xl font-extrabold mb-4 capitalize italic font-sans">
-            You speak and we help you execute it in your own way!! Welcome To
+          <h1 className="text-2xl text-green-700 lg:text-3xl font-extrabold mb-4 capitalize italic font-sans">
+            Welcome To
             Velisia Web Development Agency.
           </h1>
           <p className="text-base mb-4">
             WebDev have experts who are experienced and knows how and what
             specification of a company or a product are to be showcased.
           </p>
-          <Flex gap="4">
-            <Button>
+          <Flex gap="4" className='mt-10 lg:mt-32  flex flex-col justify-center '>
+            <Button variant="velisa">
               <Link href="/contact">Get Started</Link>
             </Button>
             <Button variant="link">Know More</Button>
           </Flex>
         </Box>
-        <Box className=" flex-1 hidden lg:block">
+        <Box className=" flex-1 hidden lg:flex">
           <Image
-            src="/hero1.jpg"
+            src={Hero}
             alt="web development"
             loading="eager"
             width="600"
